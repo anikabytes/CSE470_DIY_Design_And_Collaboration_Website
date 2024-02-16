0bb1811eb902d4203e27406eca4a33052d2a4c2f
Anika Islam <anikaislamporoma17@gmail.com>
2024-02-17 02:12:18 +0600

    sprint1_Clothes UI update

--- frontend/src/screens/Clothes.jsx
+++ frontend/src/screens/Clothes.jsx
@@ -1,11 +1,14 @@
-
-
 const Clothes = () => {
+  return (
+    <div className="w-screen h-screen bg-blue-200">
+      <div className=" text-center text-3xl text-red-500 py-10 ">
+        View Designs 
+      </div>
+      
+    </div>
 
-  <div>
-    Clothes
-  </div>
-};
+  )
+}
 
-export default Clothes;
+export default Clothes 
 
