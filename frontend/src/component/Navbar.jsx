bafd1a80b80cd97f97ebbb9e6c57461f03f2dd2d
Anika Islam <anikaislamporoma17@gmail.com>
2024-02-17 00:49:59 +0600

    Create Navbar.jsx
    sprint1_navbar

---
+++ frontend/src/component/Navbar.jsx
@@ -0,0 +1,116 @@
+
+import {useState} from "react";
+import { FaTimes } from "react-icons/fa";
+import { CiMenuFries } from "react-icons/ci";
+
+
+const Navbar = () => {
+
+    const [click, setClick] = useState(false);
+    const handleClick = () => setClick(!click);
+
+
+    // const ClothesDropdown = () => {
+
+    //     const [showDropdown, setShowDropdown] = useState(false);
+
+    //     const handleDropdownToggle = () => {
+    //         setShowDropdown(!showDropdown);
+    //     };
+    // }
+
+    const content = <>
+        <div className="lg:hidden block absolute top-16 w-full left-0 right-0 bg-orange-300 transition">
+            <ul className="text-center text-xl p-20">
+                <CustomLink spy={true} smooth={true}  href = '/home'>
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-yellow-200 hover:rounded">
+                        About
+                    </li>
+                </CustomLink>
+                
+                <CustomLink spy={true} smooth={true} href="/clothes">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-yellow-200 hover:rounded">
+                        Clothes 
+                    </li>
+                </CustomLink>
+
+                <CustomLink spy={true} smooth={true}  href="/purchase">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-yellow-200 hover:rounded">
+                        Purchase
+                    </li>
+                </CustomLink>
+
+                <CustomLink spy={true} smooth={true}  href="/profile">
+                    <li className="my-4 py-4 border-b border-slate-800 hover:bg-yellow-200 hover:rounded">
+                        Profile
+                    </li>
+                </CustomLink>
+            </ul>
+
+        </div>
+    </>
+
+    return (
+            <nav>
+               <div className="h-10vh flex justify-between z-50  text-white lg:py-5 px-20 py-4 transition bg-orange-300">
+                    <div className="flex items-center flex-1">
+                        <span className="text-3xl font-bold">
+                            ARTisTs
+                        </span>
+                    </div>
+                    <div className="lg:flex md:flex lg:flex-1 times center justify-end font-normal hidden">
+                        <div className="flex-10">
+                            <ul className="flex gap-8 mr-16 text-[18px]">
+                                <CustomLink spy={true} smooth={true} href = "\home">
+                                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                                        Home 
+                                    </li>
+                                </CustomLink>
+                                
+                                <CustomLink spy={true} smooth={true} href ="\clothes">
+                                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer ">
+                                        Clothes
+                                    </li>
+                                </CustomLink>
+
+                                <CustomLink spy={true} smooth={true} href='\purchase'>
+                                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                                        Purchase 
+                                    </li>
+                                </CustomLink>
+
+                                <CustomLink spy={true} smooth={true} href='profile'>
+                                    <li className="hover:text-fuchsia-600 transition border-b-2 border-slate-900 hover:border-fuchsia-600 cursor-pointer">
+                                        Profile
+                                    </li>
+                                </CustomLink>
+                            </ul>
+                        </div>
+                    </div>
+                    
+                        <div>
+                            {click && content}
+                        </div>
+
+                        <button className="block sm:hidden transition" onClick={handleClick}>
+                            {click ? <FaTimes/> : <CiMenuFries/>}
+                        </button>
+                </div> 
+            </nav>
+    )
+}
+
+function CustomLink ({href, children, ...props}) {
+    const path = window.location.pathname 
+    return (
+        <li className = {path === href ? active : ""}>
+            <a href={href} {...props}>
+                {children}
+            </a>
+        </li>
+    )
+}
+
+export default Navbar
+
+
