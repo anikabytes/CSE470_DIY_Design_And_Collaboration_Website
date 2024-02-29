1640e637a6947164e7fd85aa0a82b86fc956b943
risatrahaman <risat.rahaman@gmail.com>
2024-02-29 23:45:09 +0600

    sprint2

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,10 @@
 import Login from './screens/Login'
 import Register from './screens/Register'
 import Profile from './screens/Profile';
+import Clothes from './screens/Clothes';
+import Purchase from './screens/Purchase';
+import Home from './screens/Home';
+
 import {
   BrowserRouter,
   Routes,
@@ -9,16 +13,27 @@ import {
 import UpdateProfile from './screens/UpdateProfile';
 
 function App() {
+
   return (
+    <div>
+
     <BrowserRouter>
       <Routes>
+
         <Route path='/login' element={<Login />}/>
         <Route path='/register' element={<Register />}/>
         <Route path='/profile' element={<Profile />}/>
         <Route path='/updateProfile' element={<UpdateProfile />}/>
+        <Route path='/' element={<Home />}/>
+        <Route path='/clothes' element={<Clothes />}/>
+        <Route path='/purchase' element={<Purchase />}/>
+
+
       </Routes>
     </BrowserRouter>
+    </div>
   )
 }
 
 export default App
+
---
+++ frontend/src/screens/Clothes.jsx
@@ -0,0 +1,17 @@
+import Navbar from "../component/Navbar.jsx"
+
+const Clothes = () => {
+  return (
+    <div className="w-screen h-screen bg-blue-200">
+      <Navbar />
+      <div className=" text-center text-3xl text-red-500 py-10 ">
+        View Designs 
+      </div>
+      
+    </div>
+
+  )
+}
+
+export default Clothes 
+
