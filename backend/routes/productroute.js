b8e6bdc006e0e726d35be92b8575f5a3ac19f661
Tanjil <99034559+Kr9nos@users.noreply.github.com>
2024-03-28 20:00:37 +0600

    Update productroute.js

--- backend/routes/productroute.js
+++ backend/routes/productroute.js
@@ -42,26 +42,6 @@ router.get('/search', (request, response) => {
             response.status(500).json({ error: 'Failed to search for products' });
         });
 });
-//   app.post('/books', async (request, response) => {
-//     try {
-//         if (
-//             !request.body.title ||
-//             !request.body.author ||
-//             !request.body.publishYear
-//         ) {
-//             return response.status(400).send({message: 'Send all the required fields: title, author, publishYear,'});
-//         }
-//             const newBook= {
-//                 title: request.body.title,
-//                 author: request.body.author,
-//                 publishYear: request.body.publishYear,
-//             };
-//             const book= await Book.create(newBook);
-//             return response.status(201).send(book);
-//     } catch(error) {
-//         // console.log(error.message);
-//         response.status(500).send({message: error.message});
-//     }
-// });
+
 
 export default router;
