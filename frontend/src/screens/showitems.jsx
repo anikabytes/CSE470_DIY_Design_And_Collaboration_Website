64bf346f8d53ce76f0f42a3e09a60b9c74de10dd
Tanjil <99034559+Kr9nos@users.noreply.github.com>
2024-03-28 20:05:29 +0600

    Update showitems.jsx

--- frontend/src/screens/showitems.jsx
+++ frontend/src/screens/showitems.jsx
@@ -2,9 +2,9 @@ import React, { useEffect, useState} from 'react';
 import axios from 'axios';
 import Spinner from '../components/Spinner';
 import { Link } from 'react-router-dom';
-import { AiOutlineEdit } from 'react-icons/ai';
-import { BsInfoCircle } from 'react-icons/bs';
-import { MdOutlineAddBox, MdOutlineDelete } from 'react-icons/md';
+import {AiOutlineEdit} from 'react-icons/ai';
+import {BsInfoCircle} from 'react-icons/bs';
+import {MdScreenSearchDesktop,MdOutlineDelete} from 'react-icons/md';
 
 const ShowItems = () => {
   const [items, setItems] = useState([]);
@@ -12,7 +12,7 @@ const ShowItems = () => {
 
   useEffect(() => {
     setLoading(true);
-    axios.get('http://localhost:2489/dress')
+    axios.get('http://localhost:3000/dress')
       .then((response) => {
         setItems(response.data.data);
         setLoading(false);
@@ -26,64 +26,44 @@ const ShowItems = () => {
   return (
     <div className='p-4'>
       <div className='flex justify-between items-center'>
-        <h1 className='text-3xl my-8'>Items list</h1>
-        <Link to='/dress/addproduct'>
-          <MdOutlineAddBox className='text-sky-800 text-4xl' />
-        </Link>
+        <h1 className='text-3xl my-8'>Products list</h1>
+        <div className='flex items-center'>
+          <div className='flex items-center mr-2'>
+            <input
+              type='text'
+              placeholder='Search products'
+              className='border-2 border-gray-300 rounded-lg px-4 py-1 focus:outline-none focus:border-sky-500'
+            />
+            <MdScreenSearchDesktop className='text-sky-800 text-4xl' />
+          </div>
+        </div>
       </div>
       {loading ? (
         <Spinner />
       ) : (
-        <table className='w-full border-separate border-spacing-2'>
-          <thead>
-            <tr>
-              <th className='border border-slate-600 rounded-md'>No</th>
-              <th className='border border-slate-600 rounded-md'>Name</th>
-              <th className='border border-slate-600 rounded-md'>Description</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Price</th>
-              <th className='border border-slate-600 rounded-md'>Image</th>
-              <th className='border border-slate-600 rounded-md max-md:hidden'>Designer</th>
-              <th className='border border-slate-600 rounded-md'>Operation</th>
-            </tr>
-          </thead>
-          <tbody>
-          {items.map((item, index) => (
-  <Link key={item._id} to={`/dress/details/${item._id}`}>
-    <tr className='h-8'>
-      <th className='border border-slate-700 rounded-md text-center'>
-        {index + 1}
-      </th>
-      <td className='border border-slate-700 rounded-md text-center'>
-        {item.name}
-      </td>
-      <td className='border border-slate-700 rounded-md text-center'>
-        {item.description}
-      </td>
-      <td className='border border-slate-700 rounded-md text-center max-md:hidden'>
-        {item.price}
-      </td>
-      <td className='border border-slate-700 rounded-md text-center'>
-        {item.productImages}
-      </td>
-      <td className='border border-slate-700 rounded-md text-center'>
-        {item.designedby}
-      </td>
-      <td className='border border-slate-700 rounded-md text-center'>
-        <div className='flex justify-center gap-x-4'>
-          <BsInfoCircle className='text-2xl text-green-800' />
-          <Link to={`/dress/colaborate/${item._id}`}>
-            <AiOutlineEdit className='text-2xl text-yellow-600' />
-          </Link>
-          <Link to={`/dress/delete/${item._id}`}>
-            <MdOutlineDelete className='text-2xl text-red-600' />
-          </Link>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4'>
+          {items.map((item) => (
+            <div key={item._id} className='border border-slate-600 rounded-md mb-4' style={{flexBasis: '33%'}}>
+              <div className='p-2'>
+              <p className='text-center'><strong>Image:</strong> {item.productImages}</p>
+                <p className='text-center mb-1'><strong>Name:</strong> {item.name}</p>
+                <p className='text-center mb-1'><strong>Description:</strong> {item.description}</p>
+                <p className='text-center mb-1'><strong>Price:</strong> {item.price}</p>
+                <div className='flex justify-center mt-2'>
+                  <Link to={`/dress/details/${item._id}`}>
+                    <BsInfoCircle className='text-2xl text-green-800 mr-2' />
+                  </Link>
+                  <Link to={`/dress/colaborate/${item._id}`}>
+                    <AiOutlineEdit className='text-2xl text-yellow-600 mr-2' />
+                  </Link>
+                  <Link to={`/dress/delete/${item._id}`}>
+                    <MdOutlineDelete className='text-2xl text-red-600' />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          ))}
         </div>
-      </td>
-    </tr>
-  </Link>
-))}
-          </tbody>
-        </table>
       )}
     </div>
   )
