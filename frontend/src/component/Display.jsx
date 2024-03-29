c16808ca70c308edb588794f5ac3e6bee4d431d6
Tanjil <99034559+Kr9nos@users.noreply.github.com>
2024-03-29 21:40:33 +0600

    Add files via upload

---
+++ frontend/src/component/Display.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { AiOutlineEdit } from 'react-icons/ai';
+import { BsInfoCircle } from 'react-icons/bs';
+import {  MdOutlineDelete } from 'react-icons/md';
+const Display = ({items}) => {
+  return (
+        items.map((item) => (
+          <Link key={item._id} to={`/clothes/details/${item._id}`}>
+            <div className='border border-slate-600 rounded-md mb-4 bg-white' style={{ flexBasis: '33%' }}>
+              <div className='p-2'>
+                <p className='text-center'><strong>Image:</strong> {item.productImages}</p>
+                <p className='text-center mb-1'><strong>Name:</strong> {item.name}</p>
+                <p className='text-center mb-1'><strong>Description:</strong> {item.description}</p>
+                <p className='text-center mb-1'><strong>Price:</strong> {item.price}</p>
+                <div className='flex justify-center mt-2'>
+                  <Link to={`/clothes/details/${item._id}`}>
+                    <BsInfoCircle className='text-2xl text-green-800 mr-2' />
+                  </Link>
+                  <Link to={`/clothes/colaborate/${item._id}`}>
+                    <AiOutlineEdit className='text-2xl text-yellow-600 mr-2' />
+                  </Link>
+                  <Link to={`/clothes/delete/${item._id}`}>
+                    <MdOutlineDelete className='text-2xl text-red-600' />
+                  </Link>
+                </div>
+              </div>
+            </div>
+          </Link>
+        ))
+  );
+};
+
+export default Display;
