268bd57e9063ccbb371260caad77158df9849837
Tanjil <99034559+Kr9nos@users.noreply.github.com>
2024-04-08 23:56:51 +0600

    Update Display.jsx

--- frontend/src/component/Display.jsx
+++ frontend/src/component/Display.jsx
@@ -9,7 +9,7 @@ const Display = ({items}) => {
           <Link key={item._id} to={`/clothes/details/${item._id}`}>
             <div className='border border-slate-600 rounded-md mb-4 bg-white' style={{ flexBasis: '33%' }}>
               <div className='p-2'>
-                <p className='text-center'><strong>Image:</strong> {item.productImages}</p>
+                <p className='text-center'><strong>Image:</strong><img src={item.productImages} alt={item.name} className="w-full" /></p>
                 <p className='text-center mb-1'><strong>Name:</strong> {item.name}</p>
                 <p className='text-center mb-1'><strong>Description:</strong> {item.description}</p>
                 <p className='text-center mb-1'><strong>Price:</strong> {item.price}</p>
