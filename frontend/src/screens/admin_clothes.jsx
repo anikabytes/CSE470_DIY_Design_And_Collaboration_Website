c9bce897e0c9ef713332a976ba9b1a401859134e
tasfiya05 <158143231+tasfiya05@users.noreply.github.com>
2024-04-18 23:28:28 +0600

    Add files via upload

---
+++ frontend/src/screens/admin_clothes.jsx
@@ -0,0 +1,14 @@
+import Admin_Nav from "../component/Admin_Nav"
+const Admin_clothes = () => {
+  return (
+    <div className="bg-yellow-100 w-screen h-screen">
+      <Admin_Nav />
+      <div className=" text-center text-3xl text-black py-10 ">
+        admin_clothes
+      </div>
+     
+    </div>
+  )
+}
+
+export default Admin_clothes;
---
+++ frontend/src/screens/admin_user.jsx
@@ -0,0 +1,14 @@
+import Admin_Nav from "../component/Admin_Nav"
+const Admin_user = () => {
+  return (
+    <div className="bg-yellow-100 w-screen h-screen">
+      <Admin_Nav />
+      <div className=" text-center text-3xl text-black py-10 ">
+        admin_user
+      </div>
+     
+    </div>
+  )
+}
+
+export default Admin_user
