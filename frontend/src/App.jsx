5ff38e6d3da6c57bcd64c56d7fdadb2d97bcd65d
Tanjil <99034559+Kr9nos@users.noreply.github.com>
2024-04-20 01:35:25 +0600

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,6 @@ import Clothes from './screens/Clothes';
 import CartPage from './screens/CartPage';
 import Home from './screens/Home';
 import Design from './screens/Design';
-import Additems from './screens/additems'
 import Deleteitem from './screens/deleteitem'
 import Searchitems from './screens/searchitems'
 import Showitems from './screens/showitems'
@@ -37,12 +36,10 @@ function App() {
         <Route path='/profile' element={<Profile />}/>
         <Route path='/updateProfile' element={<UpdateProfile />}/>
         <Route path='/' element={<Home />}/>
-        {/* <Route path='/clothes' element={<Clothes />}/> */}
         <Route path='/CartPage' element={<CartPage />}/>
         <Route path='/design' element={<Design />}/>
         <Route path="/clothes" element = {<Showitems />} />
         <Route path="/clothes/details/:id" element = {<Viewitem />} />
-        <Route path="/clothes/addproduct" element = {<Additems />} />
         <Route path="/clothes/search/:searchString" element = {<Searchitems />} />
         <Route path="/clothes/delete/:id" element = {<Deleteitem />} />
         <Route path='/checkout' element={< Checkout />}/>
