d848e2763a3bf17631a0f5701f7900f7ca22e4c6
Tanjil <99034559+Kr9nos@users.noreply.github.com>
2024-04-25 19:49:11 +0600

    Update productRoute.js

--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -3,7 +3,7 @@ import Product from "../models/productModel.js";
 
 const router = express.Router();
 
-router.get('/clothes', async (request, response) => {
+router.get('/', async (request, response) => {
     try {
       const items = await Product.find({});
   
@@ -14,6 +14,17 @@ router.get('/clothes', async (request, response) => {
     } catch (error) {
       console.log(error.message);
       response.status(500).send({ message: error.message });
+    }
+  });
+  router.get('/search/:searchString', async (request, response) => {
+    const searchString = request.params.searchString;
+    console.log(searchString);
+    const regex = new RegExp(searchString, 'i');
+    try {
+      const products = await Product.find({ name: regex });
+      response.json(products);
+    } catch (err) {
+      response.status(500).json({ message: 'Error searching for products', error: err });
     }
   });
 
