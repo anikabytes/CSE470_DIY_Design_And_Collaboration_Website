20b329b8c3c377b3a4dbd9ac1ff8aeed476c695a
Tanjil <99034559+Kr9nos@users.noreply.github.com>
2024-04-25 20:25:33 +0600

    Update admindesignpage.jsx

--- frontend/src/screens/admindesignpage.jsx
+++ frontend/src/screens/admindesignpage.jsx
@@ -27,23 +27,7 @@ const ShowItems = () => {
   return (
     <div >
       <AdminNav />
-      <div className='flex justify-between items-center p-4'>
-        <h1 className='text-3xl my-8'>Products list</h1>
-        <div className='flex items-center'>
-          <div className='flex items-center mr-2'>
-            <input
-              type='text'
-              placeholder='Search products'
-              value={searchString}
-              onChange={(searchstring) => setSearchString(searchstring.target.value)}
-              className='border-2 border-gray-300 rounded-lg px-4 py-1 focus:outline-none focus:border-sky-500'
-            />
-            <Link to={`/admin/search/${searchString}`}>
-              <MdScreenSearchDesktop className='text-sky-800 text-4xl' />
-            </Link>
-          </div>
-        </div>
-      </div>
+      
       <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4 px-4'>
       {loading ? (
         <div>
@@ -54,13 +38,6 @@ const ShowItems = () => {
         items={items}
       />)}
       </div>
-      <div className='flex justify-between items-center justify-center mt-4 py-4'>
-        <Link to = "/design">
-            <button className='bg-orange-300 px-4 py-2 rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-green-300'>
-              Customise Your Dress 
-            </button>
-        </Link>
-        </div>
     </div>
   );
 };
