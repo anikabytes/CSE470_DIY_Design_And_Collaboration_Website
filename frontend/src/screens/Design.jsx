326b8e05f28132dc6f08c3f84359c72989dab7a3
Anika Islam <anikaislamporoma17@gmail.com>
2024-04-25 18:00:14 +0600

    sprint4_design page UI updated

--- frontend/src/screens/Design.jsx
+++ frontend/src/screens/Design.jsx
@@ -38,7 +38,7 @@ const Design = () => {
   const [dressSize, setDressSize] = useState('');
   const [textColor, setTextColor] = useState(colorPalatte);
   const [text, setText] = useState('');
-  const [fontSize, setFontSize] = useState(25);
+  const [fontSize, setFontSize] = useState(50);
   const [fontStyle,setFontStyle] = useState('');
   const [textPosition, setTextPosition] = useState({ x: 50, y: 50});
   const [saveSuccess, setSaveSuccess] = useState(false);
@@ -253,113 +253,205 @@ const Design = () => {
     draggingItemRef.current=null;
   };
 
-  const sizeStyles = {
-    Male_S: { width: '150px', height: '200px' },
-    Male_M: { width: '200px', height: '250px' },
-    Male_L: { width: '250px', height: '300px' },
-    Male_XL: {width:'350px', height:'400px'},
-    
-    Female_XS: { width: '120px', height: '180px' },
-    Female_S: { width: '140px', height: '200px' },
-    Female_M: { width: '160px', height: '220px' },
-    Female_L: { width: '180px', height: '240px' },
-    Female_XL: {width:'350px', height:'400px'},
-  };
+
 
 
   return (
-    <div>
-      <Navbar />
 
-      <div className=" bg-rose-200 md-flex  justify-center items-center">
-      <div className="text-center text-3xl text-red-500 p-20">Customise Your Dress!!!</div>
-      <div className="flex flex-col items-center md-flex ">
-        <canvas 
-          ref={canvasRef} 
-          style={{ ...sizeStyles[dressSize], maxWidth: '100%', height: 'auto', marginBottom: '1rem' }}
-          onMouseDown={handleMouseDown}
-          onMouseMove={handleMouseMove}
-          onMouseUp={handleMouseUp}
-        ></canvas>
-        <input type="file" onChange={handleFileChange} className="py-2 px-4 border rounded-lg bg-white" />
-        <select value={gender} onChange={handleGenderChange} className="mt-4 px-4 py-2 border rounded-lg bg-white">
-          <option value="">Select Gender</option>
-          <option value="Male">Male</option>
-          <option value="Female">Female</option>
-        </select>
-        {gender && (
-          <select value={dressSize} onChange={handleDressSizeChange} className="mt-2 px-4 py-2 border rounded-lg bg-white">
-            <option value="">Select Size</option>
-            {gender === 'Male' && (
-              <>
-                <option value="Male_S">S</option>
-                <option value="Male_M">M</option>
-                <option value="Male_L">L</option>
-                <option value="Male_XL">XL</option>
-              </>
-            )}
-            {gender === 'Female' && (
-              <>
-                <option value="Female_XS">XS</option>
-                <option value="Female_S">S</option>
-                <option value="Female_M">M</option>
-                <option value="Female_L">L</option>
-                <option value="Female_XL">XL</option>
-              </>
-            )}
-          </select>
-        )}
-        <input type="file" onChange={handleLogoChange} className="py-2 px-4 border rounded-lg bg-white mt-4" />
-        <button onClick={() => {setLogoWidth(logoWidth+10);setLogoHeight(logoHeight + 10);}} style={{ backgroundColor: 'white', color: 'black' ,borderRadius:'10px', padding:'5px 8px', cursor:'pointer',marginTop: '10px'}}>Increase Size</button>
-        
-        <button onClick={() => {setLogoWidth(logoWidth -10);setLogoHeight(logoHeight- 10);}} style={{ backgroundColor: 'white', color: 'black' ,borderRadius:'10px', padding:'5px 8px', cursor:'pointer', marginTop:'10px',}}>Decrease Size</button>
-        <input 
-          type="text" 
-          value={text} 
-          onChange={handleTextChange} 
-          placeholder="Enter text" 
-          className="py-2 px-4 border rounded-lg bg-white mt-4" 
-        />
-        <select value={fontStyle} onChange={handleFontStyleChange} className="mt-2 px-4 py-2 border rounded-lg bg-white">
-          <option value="Arial">Arial</option>
-          <option value="Times New Roman">Times New Roman</option>
-        </select>
-        <input 
-          type="number" 
-          value={fontSize} 
-          onChange={handleFontSizeChange} 
-          placeholder="Enter font size" 
-          className="py-2 px-4 border rounded-lg bg-white mt-2" 
-        />
-        <div className="grid grid-cols-2 gap-4 p-5">
-          <SketchPicker 
-            color={textColor} 
-            onChange={handleTextColorChange}  
-          />
-
-          <SketchPicker 
-            color={dressColor} 
-            onChange={handleDressColorChange} 
-          />
+    <div className="min-h-screen bg-gradient-to-r from-amber-200 to-pink-200 flex justify-center items-center">
+      <div className=" bg-white rounded-lg shadow-lg shadow-black p-9 max-w-6xl w-full">
+        <div className="text-center text-4xl font-bold text-amber-500 mb-8">
+          Customize Your Dress
         </div>
 
-        <button onClick={handleSave} className="mt-4 bg-orange-300 text-white px-6 py-2 rounded-md hover:bg-yellow-200 focus:outline-none focus:ring-2 focus:ring-green-300">
-          Save
-        </button>
-        {savePrompt && (
-          <div className="mt-4">
-            <p>Are you sure you want to save?</p>
-            <button onClick={confirmSave} className="bg-green-400 text-white px-4 py-2 rounded-md mr-2">Yes</button>
-            <button onClick={handleCancelSave} className="bg-red-400 text-white px-4 py-2 rounded-md">No</button>
+        <div className="flex flex-col md:flex-row md:items-center">
+
+          <div className="md:w-1/3 md:mr-8 ">
+            <canvas
+              ref={canvasRef}
+              style={{
+                
+                maxWidth: '100%',
+                height: '100%',
+                marginBottom: '1rem',
+              }}
+              onMouseDown={handleMouseDown}
+              onMouseMove={handleMouseMove}
+              onMouseUp={handleMouseUp}
+              className="shadow-lg rounded-lg mx-auto shadow-black"
+            />
           </div>
-        )}
-        {saveSuccess && (
-          <div className="mt-4 text-green-400">Saved successfully!</div>
-        )}
+
+          <div className="md:w-1/2">
+            <div className="flex flex-col md:flex-row md:items-center font-bold text-lg text-amber-500 ">
+                <div className="px-4 py-2 md:ml-4 mt-4">
+                  Upload Image 
+                </div>
+                <div className="px-4 py-2 md:ml-2 mt-4">
+                  
+                </div>
+                <div className="px-4 py-2 md:ml-20 mt-4">
+                  Upload Logo 
+                </div>
+            </div>
+
+            <div className="flex flex-col md:flex-row md:items-center">
+              <input
+                type="file"
+                
+                onChange={handleFileChange}
+                className="py-2 px-4 border rounded-lg bg-amber-100 mt-4 md:mr-4 focus:outline-none focus:ring-3 focus:ring-amber-500"
+              />
+
+              <input
+                type="file"
+                onChange={handleLogoChange}
+                className="mt-4 px-4 py-2 border rounded-lg bg-amber-100 md:mx-4 focus:outline-none focus:ring-2 focus:ring-amber-500"
+              />  
+              
+
+
+            </div>
+
+            <div className="flex items-center  mb-4">
+              <select
+                  value={gender}
+                  onChange={handleGenderChange}
+                  className="mt-4 px-4 py-2 border rounded-lg bg-amber-100 md:mx-4 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                >
+                  <option value="">Select Gender</option>
+                  <option value="Male">Male</option>
+                  <option value="Female">Female</option>
+                </select>
+                {gender && (
+                  <select
+                    value={dressSize}
+                    onChange={handleDressSizeChange}
+                    className="mt-4 px-4 py-2 border rounded-lg bg-amber-100 md:mx-4 focus:outline-none focus:ring-2 focus:ring-amber-500"
+                  >
+                    <option value="">Select Size</option>
+                    {gender === 'Male' && (
+                      <>
+                        <option value="Male_S">S</option>
+                        <option value="Male_M">M</option>
+                        <option value="Male_L">L</option>
+                        <option value="Male_XL">XL</option>
+                      </>
+                    )}
+                    {gender === 'Female' && (
+                      <>
+                        <option value="Female_XS">XS</option>
+                        <option value="Female_S">S</option>
+                        <option value="Female_M">M</option>
+                        <option value="Female_L">L</option>
+                        <option value="Female_XL">XL</option>
+                      </>
+                    )}
+                  </select>
+                )}
+            </div>
+
+            <div className="flex items-center mb-4">
+
+              <button
+                onClick={() => {
+                  setLogoWidth(logoWidth + 10);
+                  setLogoHeight(logoHeight + 10);
+                }}
+                className="bg-amber-500 text-white rounded-lg px-4 py-2 mr-2 hover:bg-amber-400 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-amber-500"
+              >
+                Increase Size
+              </button>
+              <button
+                onClick={() => {
+                  setLogoWidth(logoWidth - 10);
+                  setLogoHeight(logoHeight - 10);
+                }}
+                className="bg-amber-500 text-white rounded-lg px-4 py-2 mr-2 hover:bg-amber-400 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-amber-500"
+              >
+                Decrease Size
+              </button>
+            </div>
+
+            <input
+              type="text"
+              value={text}
+              onChange={handleTextChange}
+              placeholder="Enter text"
+              className="py-2 px-4 border rounded-lg bg-amber-100 mb-4 w-full focus:outline-none focus:ring-2 focus:ring-amber-500"
+            />
+
+            <div className="flex flex-col md:flex-row mb-4">
+              <select
+                value={fontStyle}
+                onChange={handleFontStyleChange}
+                className="px-4 py-2 border rounded-lg bg-amber-100 md:mr-4 focus:outline-none focus:ring-2 focus:ring-amber-500"
+              >
+                <option value="Arial">Arial</option>
+                <option value="Times New Roman">Times New Roman</option>
+                <option value="Verdana">Verdana</option>
+                <option value="Helvetica">Helvetica</option>
+                <option value="Courier New">Courier New</option>
+                <option value="Georgia">Georgia</option>
+                <option value="Tahoma">Tahoma</option>
+                <option value="Trebuchet MS">Trebuchet MS</option>
+                <option value="Comic Sans MS">Comic Sans MS</option>
+              </select>
+              <input
+                type="number"
+                value={fontSize}
+                onChange={handleFontSizeChange}
+                placeholder="Enter font size"
+                className="py-2 px-4 border rounded-lg bg-amber-100 mt-2 md:mt-0 md:ml-4 focus:outline-none focus:ring-2 focus:ring-amber-500"
+              />
+            </div>
+
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-4 p-5">
+              <SketchPicker
+                color={textColor}
+                onChange={handleTextColorChange}
+                className="shadow-lg rounded-lg"
+              />
+              <SketchPicker
+                color={dressColor}
+                onChange={handleDressColorChange}
+                className="shadow-lg rounded-lg"
+              />
+            </div>
+
+            <button
+              onClick={handleSave}
+              className="mt-4 bg-amber-500 text-white px-6 py-2 rounded-md hover:bg-amber-400 focus:outline-none focus:ring-2 focus:ring-orange-200 transition-colors duration-300"
+            >
+              Save
+            </button>
+
+            {savePrompt && (
+              <div className="mt-4 flex items-center justify-center">
+                <p className="mr-4">Are you sure you want to save?</p>
+                <button
+                  onClick={confirmSave}
+                  className="bg-green-500 text-white px-4 py-2 rounded-md mr-2 hover:bg-green-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-green-400"
+                >
+                  Yes
+                </button>
+                <button
+                  onClick={handleCancelSave}
+                  className="bg-red-500 text-white px-4 py-2 rounded-md hover:bg-red-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-red-400"
+                >
+                  No
+                </button>
+              </div>
+            )}
+
+            {saveSuccess && (
+              <div className="mt-4 text-green-500 font:bold text-center">Saved successfully!</div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
-    </div>
-    
+        
   );
 };
 
