6189602310340df6c9d7b5581f387598a06fa904
Anika Islam <anikaislamporoma17@gmail.com>
2024-04-25 22:40:48 +0600

    user and admin homepage updated

--- frontend/src/screens/AdminHome.jsx
+++ frontend/src/screens/AdminHome.jsx
@@ -1,18 +1,17 @@
-import Navbar from "../component/AdminNav.jsx"
+import AdminNav from '../component/AdminNav';
+import homepageImage from "../assets/welcome.jpg";
 import { useState, useEffect } from 'react';
 
-const AdminHome = () => {
-  
-
-  return (
-    <div className="bg-yellow-100 w-screen h-screen">
-      <Navbar />
-      <div className=" text-center text-3xl text-black py-10 ">
-        Admin Panel
-      </div>
-     
-    </div>
-  )
+const Home = () => {
+ return (
+   <div className="bg-yellow-100 min-h-screen flex flex-col">
+     <AdminNav />
+     <div className="flex-grow flex flex-col items-center justify-center text-center text-3xl text-amber-500 font-bold py-10">
+      Welcome To ARTisTs (Admin Panel)
+       <img src={homepageImage}  className="mx-auto my-4 max-w-full h-auto w-50" />
+     </div>
+   </div>
+ )
 }
 
-export default AdminHome
+export default Home
--- frontend/src/screens/Home.jsx
+++ frontend/src/screens/Home.jsx
@@ -1,18 +1,17 @@
-import Navbar from "../component/Navbar.jsx"
+import Navbar from "../component/Navbar.jsx";
+import homepageImage from "../assets/welcome.jpg";
 import { useState, useEffect } from 'react';
 
 const Home = () => {
-  
-
-  return (
-    <div className="bg-yellow-100 w-screen h-screen">
-      <Navbar />
-      <div className=" text-center text-3xl text-black py-10 ">
-        Home page
-      </div>
-     
-    </div>
-  )
+ return (
+   <div className="bg-yellow-100 min-h-screen flex flex-col">
+     <Navbar />
+     <div className="flex-grow flex flex-col items-center justify-center text-center text-3xl text-amber-500 font-bold py-10">
+      Welcome To ARTisTs
+       <img src={homepageImage}  className="mx-auto my-4 max-w-full h-auto w-50" />
+     </div>
+   </div>
+ )
 }
 
 export default Home
